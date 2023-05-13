9ec279a5a7cb8775df842c2a24dfd97cbbdd5a0e
LujanSolo <lujansolo@proton.me>
2023-05-13 01:01:47 -0700

    imported firebase utilities and functions

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
+import {
+  getDatabase,
+  ref,
+  onValue
+} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
+
+const firebaseConfig = {
+  databaseURL: "https://shoutout-a4642-default-rtdb.firebaseio.com/",
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const endorsementsDB = ref(database, "endorsements");
+
 const endorseInputEl = document.getElementById("endorsement");
 const fromEl = document.getElementById("from-field");
 const toEl = document.getElementById("to-field");
 
 const publishBtn = document.getElementById("publish-btn");
+const endorseResults = document.getElementById("output-container");
+const heartIconBtn = document.getElementById("heart-icon");
+
 
 publishBtn.addEventListener("click", () => {
-  console.log('clicked')
 
   // send the info from the 3 fields to firebase server
   // using those values from firebase, populate the necessary fields in the "endorse-box"
 
-  // <div class="endorse-box">
-  //     <h3 class="endorse-to">To Example</h3>
-  //     <p class="endorse-paragraph">Example Text of how great someone is today!</p>
-  //     <h3 class="endorse-from">From Me</h3>
-  //     <p id="heart-icon">❤️ <span id="heart-count">4</span></p>
+  endorseResults.innerHTML = `<div class="endorse-box">
+      <h3 class="endorse-to">To ${toValue}</h3>
+      <p class="endorse-paragraph">${endorseValue}</p>
+      <h3 class="endorse-from">From ${fromValue}</h3>
+      <button id="heart-icon">❤️ <span id="heart-count">${heart - count}</span></button> //* add eventlistener to span
+    </div>`;
+})
 
-  //   </div>
+heartIconBtn.addEventListener("click", () => {
+  let heartCountEl = document.getElementById("heart-count");
+
 })
